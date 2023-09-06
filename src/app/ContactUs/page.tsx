32fbb15d7e5ec7fb319c02520aa18c9c8a8acd7a
Balkis20 <bilalbalkisbegam@gmail.com>
2023-09-07 00:10:49 +0530

    Initial Setup

---
+++ src/app/ContactUs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      ContactUs
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Home from './Home/Page'
+
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
