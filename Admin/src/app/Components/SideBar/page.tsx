46fb19a5ed9da4e4106bd9b96a9caffa1213935d
ArulKumar8270 <arulkumar8270@gmail.com>
2023-12-12 01:24:09 +0530

    update

--- Admin/src/app/Components/SideBar/page.tsx
+++ Admin/src/app/Components/SideBar/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
 
@@ -5,17 +6,22 @@ export const SideNavbar = () => {
   return (
     <>
       <div className="navBarStyle">
-        <a
+        <Link
           href="#"
           className="flex items-center justify-center border-b-2 border-b-white text-gray-900 dark:text-white group logoCls"
         >
-          <span className="">Logo</span>
-        </a>
-        <aside id="default-sidebar" className="h-[84vh]" aria-label="Sidebar">
+          <span className="">
+            <Image src={"/img/logo.png"} alt="logo" width={80} height={80} />
+          </span>
+        </Link>
+        <aside id="default-sidebar" className="h-[80vh]" aria-label="Sidebar">
           <div className="h-full px-3 pb-4 pt-0 overflow-y-auto custom-scrollbar">
             <div className="scroll-content h-fit left-0 top-0 transition-transform z-40">
               <ul role="list" className="space-y-2 font-medium list-disc">
-                <li className="rounded-xl"  style={{ backgroundColor: "var(--secondary-500)" }}>
+                <li
+                  className="rounded-xl"
+                  style={{ backgroundColor: "var(--secondary-500)" }}
+                >
                   <Link
                     href="/"
                     className="mt-7 p-3 text-sm flex items-center text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
